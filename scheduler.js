2115d9ee9cbf62132bd8c8656ed1c6143fa0ab20
dchotzen-hartzell <dchotzen-hartzell19@punahou.edu>
2016-08-23 21:22:51 -1000

    Revert "Revert "Tidy up JS; Add place for class name/location/time""
    This reverts commit 8cdb772ec1bf64a7b36eba4437a3648e5a5f96bb.

--- scheduler.js
+++ scheduler.js
@@ -1,3 +1,5 @@
+
+// Creates schedule table
 for (var i = 0; i < 32; i++) {
   var row = document.createElement("tr")
   row.setAttribute('id', 'row-' + i)
@@ -8,12 +10,7 @@ for (var i = 0; i < 32; i++) {
   document.getElementById("table").appendChild(row)
 
   var time = document.createElement("th")
-  var min = (i * 15) + 450
-  var leftside = min % 60
-  if (leftside == 0) {
-    leftside = '00'
-  }
-  time.innerHTML = Math.floor(min / 60) + ":" + leftside
+  time.innerHTML = toTime(i)
   document.getElementById("row-" + i).appendChild(time)
 
   for (var j = 0; j < 6; j++) {
@@ -22,4 +19,30 @@ for (var i = 0; i < 32; i++) {
     timeslot.setAttribute('class', 'timeslot')
     row.appendChild(timeslot)
   }
+}
+
+var startTimeSelect = document.getElementById("start-time-select")
+var endTimeSelect = document.getElementById("end-time-select")
+
+for (var i = 0; i < 32; i++) {
+  var soption = document.createElement("option")
+  var eoption = document.createElement("option")
+
+  soption.value = i
+  eoption.value = i+(2/3)
+
+  soption.innerHTML = toTime(i)
+  eoption.innerHTML = toTime(i+(2/3))
+
+  startTimeSelect.appendChild(soption)
+  endTimeSelect.appendChild(eoption)
+}
+
+function toTime(period) {
+  var min = (period * 15) + 450
+  var leftside = min % 60
+  if (leftside == 0) {
+    leftside = '00'
+  }
+  return Math.floor(min / 60) + ":" + leftside
 }
