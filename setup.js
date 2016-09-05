6efdd75f9265fa33e94ce75e5ae8d547e04c57d5
dchotzen-hartzell <dchotzen-hartzell19@punahou.edu>
2016-09-04 15:23:38 -1000

    Make table and format it to scroll

---
+++ setup.js
@@ -0,0 +1,40 @@
+var letters = ["A", "B", "C", "D", "E", "F"];
+
+function toTime(period) {
+  var min = (period * 15) + 450
+  var leftside = min % 60
+  if (leftside == 0) {
+    leftside = '00'
+  }
+  return Math.floor(min / 60) + ":" + leftside
+}
+// Copy below this line:
+/*---------------------------------------------------------------*/
+
+setup();
+function setup() {
+  var timesTable = document.getElementById("times-table")
+  var schedTable = document.getElementById("sched-table")
+
+  for (var i = 0; i < 32; i++) {
+    var timesRow = document.createElement("tr")
+    var timesH = document.createElement("th")
+    timesH.innerHTML = toTime(i)
+    timesH.style = "border-top: none; border-left: 2px solid black; border-right: 2px solid black"
+
+    timesRow.appendChild(timesH)
+    timesTable.appendChild(timesRow)
+
+    var schedRow = document.createElement("tr")
+
+    for (var j = 0; j < 6; j++) {
+      var timeslot = document.createElement("td")
+      timeslot.setAttribute('id', 'timeslot-' + i + '-' + j)
+      timeslot.setAttribute('class', 'timeslot')
+      schedRow.appendChild(timeslot)
+    }
+
+    schedTable.appendChild(schedRow)
+  }
+
+}
