ec49a7a6714a350ee4d6e366c916490b4e51a598
dchotzen-hartzell <dchotzen-hartzell19@punahou.edu>
2016-09-04 17:19:17 -1000

    Do the first two panels of the form

--- setup.js
+++ setup.js
@@ -20,8 +20,12 @@ function setup() {
     var timesRow = document.createElement("tr")
     var timesH = document.createElement("th")
     timesH.innerHTML = toTime(i)
-    timesH.style = "border-top: none; border-left: 2px solid black; border-right: 2px solid black"
     timesH.id = "timelabel-" + i
+    if (i % 2 === 0) {
+      timesH.style = "border-top: none; border-left: 2px solid black; border-right: 2px solid black; background-color: #FFFFFF"
+    } else {
+      timesH.style = "border-top: none; border-left: 2px solid black; border-right: 2px solid black; background-color: #D3D3D3"
+    }
 
     timesRow.appendChild(timesH)
     timesTable.appendChild(timesRow)
@@ -32,11 +36,43 @@ function setup() {
       var timeslot = document.createElement("td")
       timeslot.setAttribute('id', 'timeslot-' + i + '-' + j)
       timeslot.setAttribute('class', 'timeslot')
+      if (i % 2 === 0) {
+        timeslot.style = "background-color: #FFFFFF"
+      } else {
+        timeslot.style = "background-color: #D3D3D3"
+      }
       schedRow.appendChild(timeslot)
     }
 
     schedTable.appendChild(schedRow)
   }
-  document.getElementById("timelabel-31").style = "border-top: none; border-left: 2px solid black; border-right: 2px solid black; border-bottom: 2px solid black"
+  document.getElementById("timelabel-31").style = "border-top: none; border-left: 2px solid black; border-right: 2px solid black; border-bottom: 2px solid black; background-color: #D3D3D3"
+
+  var startTimeSelect = document.getElementById("start-time-select")
+  var endTimeSelect = document.getElementById("end-time-select")
+
+  for (var i = 0; i < 32; i++) {
+    var soption = document.createElement("option")
+    var eoption = document.createElement("option")
+
+    soption.value = i
+    eoption.value = i+1
+
+    soption.id = "soption-" + soption.value
+    eoption.id = "eoption-" + eoption.value
+
+    if (i === 3) {
+      eoption.selected = "selected"
+    }
+
+    soption.innerHTML = toTime(i)
+    eoption.innerHTML = toTime(i+(2/3))
+
+    startTimeSelect.appendChild(soption)
+    endTimeSelect.appendChild(eoption)
+  }
+
+
+
 
 }
